f026ff0c3c9b0cfe85560b0fc2ed35f575ac8432
unknown <sogand.azadi82@gmail.com>
2024-07-21 15:37:57 -0700

    static files

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 const port = 3000;
 
 
+app.use("/static" , express.static(path.join(__dirname , "public")))
+
+
 app.get("/" , (req , res)=>{
-    res.send("Hey , This is Sogand")
+    res.sendFile(path.join(__dirname , "public" , "images" , "images.jpg"))
 });
+
 
 app.listen(port , () =>{
     console.log(`Listeninig on port ${port}`)
