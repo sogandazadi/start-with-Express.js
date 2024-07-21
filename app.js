7c9a01a910b3c26a6fcc9d4d3da8423d07d7772c
unknown <sogand.azadi82@gmail.com>
2024-07-21 14:55:11 -0700

    app respond to (/)

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+
+app.get("/" , (req , res)=>{
+    res.send("Hey , This is Sogand")
+});
+
 app.listen(port , () =>{
     console.log(`Listeninig on port ${port}`)
 });
